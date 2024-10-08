af05e10ed6041ece137ca7b09435da59a2346903
Ron Pang <lokman402@gmail.com>
2024-10-08 14:56:08 +0800

    Add files via upload

--- udp.py
+++ udp.py
@@ -1,9 +1,12 @@
 import socket
+import json
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', 1460))
 
 while True:
     message, address = server_socket.recvfrom(1024)
-    message = message.upper()
+    print(message)
+    message.decode("utf-8")
+    print(json.loads(message[op]))
     server_socket.sendto(message, address)
