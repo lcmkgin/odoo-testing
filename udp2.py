97a386d5f26818316af8118ef4560f081d98efbe
Ron Pang <lokman402@gmail.com>
2024-10-09 17:41:57 +0800

    Add files via upload

--- udp2.py
+++ udp2.py
@@ -38,10 +38,10 @@ while True:
             temp = count + "}"
         result = json.loads(temp)
         data.append(result)
-        if data[data.index(count)]["op"] == x or data[data.index(count)]["op"].find(x) != -1:
+        if data[info.index(count)]["op"] == x or data[info.index(count)]["op"].find(x) != -1:
             print ("Operation mode = ",x)
             if x == "gpm" or x == "gwm":
-                print ("ID = ", data[data.index(count)]["id"])
+                print ("ID = ", data[info.index(count)]["id"])
     # try:
     #     message, address = server_socket.recvfrom(1024)
     #     print(message)
