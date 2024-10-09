e8ddb5fd4008bf8fe78bf380856ddf26739636df
Ron Pang <lokman402@gmail.com>
2024-10-09 15:05:14 +0800

    Add files via upload

--- udp.py
+++ udp.py
@@ -31,14 +31,12 @@ while True:
         message.decode("utf-8")
         data = json.loads(message)
         for x in ops:
-            print (x)
-            #print (data["op"])
-            if data["op"] == x:
+            if data["op"] == x or data["op"].index(x) != -1:
                 print ("Operation mode = ",x)
-            elif data["op"].find(ops[1]) != -1:
-                print ("Operaton mode =", ops[1]+data["id"])
-            elif data["op"].find(ops[2]) != -1:
-                print ("Operation mode =", ops[2]+data["id"])
+                # elif data["op"].find(ops[1]) != -1:
+                #     print ("Operaton mode =", ops[1]+data["id"])
+                # elif data["op"].find(ops[2]) != -1:
+                #     print ("Operation mode =", ops[2]+data["id"])
         #print (data["op"])
         server_socket.sendto(message, address)
         #models.execute_kw(db, uid, password, 'hr.employee', 'write', [[21], {'work_email': data["op"], 'work_phone': data["type"]}])
